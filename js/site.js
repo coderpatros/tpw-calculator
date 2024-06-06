1188f728ca6d7f9530a5373d378b016beb7b9bd3
Kraksna83 <43823358+Kraksna83@users.noreply.github.com>
2024-06-06 14:57:04 +0200

    Added water calculation

--- js/site.js
+++ js/site.js
@@ -18,6 +18,7 @@
             var newValue = finalWashVolume * multiplier;
             $field.val(roundNumberByStepValue(newValue, fieldStep));
         });
+        waterChangeHandler();
     };
 
     function otherIngredientChangeHandler()
@@ -30,11 +31,25 @@
         $metricFinalWashVolume.change();
     };
     
+    function waterChangeHandler()
+    {
+		var totalVolume = Number(document.getElementById('metricFinalWashVolume').value)
+		//volume of all ingredients. 
+		var totalSugar = Number(document.getElementById('metricSugar').value) / 1.58 //sacharose density = 1.58
+		var totalTomato = Number(document.getElementById('metricTomatoPaste').value) /1120 // tomato paste density = 1.12
+		var totalLemon = Number(document.getElementById('metricLemonJuice').value) / 1000
+		var remainingWater = totalVolume - (totalSugar + totalTomato + totalLemon)
+		document.getElementById('metricWater').value = roundNumberByStepValue(remainingWater, 0.1)
+		document.getElementById('imperialWater').value = roundNumberByStepValue(remainingWater * 0.264172052, 0.1)
+    };
+    
 
     // hookup event handlers
     $metricFinalWashVolume.change(metricFinalWashVolumeChangeHandler);
     $metricFinalWashVolume.on('input', metricFinalWashVolumeChangeHandler);
     metricFinalWashVolumeChangeHandler();
+    
+    
 
     $outputs.each(function(index, field) {
         var $field = $(field);
